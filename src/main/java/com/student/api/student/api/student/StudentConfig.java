cd65976454161a40c7744a9335e4bd39b7fa6fe2
SaberHardy <samirsamado5@gmail.com>
2024-07-07 13:21:38 -0700

    add student config class

---
+++ src/main/java/com/student/api/student/api/student/StudentConfig.java
@@ -0,0 +1,31 @@
+package com.student.api.student.api.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository studentRepository) {
+        return args -> {
+            Student hardy = new Student(
+                    "hardy",
+                    "hardy@gmail.com",
+                    LocalDate.of(2000, Month.FEBRUARY, 5),
+                    34);
+
+            Student don = new Student(
+                    "don",
+                    "don@gmail.com",
+                    LocalDate.of(2000, Month.JANUARY, 5),
+                    20);
+
+            studentRepository.saveAll(List.of(hardy, don));
+        };
+    }
+}
